10e3e93c6148c711d22130438cc37e7e5b86fb46
TerraBean <mujahidadams@ymail.com>
2024-06-06 13:25:25 +0000

    add user to login response

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.post('/login', async (req, res) => {
       return res.status(401).json({ message: 'Invalid username or password' });
     }
 
-    res.status(200).json({ message: 'Login successful', userId: foundUser.Id });
+    res.status(200).json({ message: 'Login successful', userId: foundUser.Id, user: foundUser});
   } catch (error) {
     console.error('Error logging in user:', error);
     res.status(500).json({ message: 'Failed to login user.' });
